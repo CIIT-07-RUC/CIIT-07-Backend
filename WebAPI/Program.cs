300a3290347aec198a3d911e74e963c6732d1e0f
Matúš Kalanin <mato.kalanin@gmail.com>
2023-11-02 20:18:18 +0100

    Configure AutoMapper  and dependency injection

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,5 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using DataLayer;
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +7,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddSingleton<IDataService, DataService>();
 
 var app = builder.Build();
 
