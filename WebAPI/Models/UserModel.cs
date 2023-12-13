cc8c7923478ce3e130866a419842ded153575141
Matúš Kalanin <mato.kalanin@gmail.com>
2023-12-13 23:51:34 +0100

    Smaller fixes

--- WebAPI/Models/UserModel.cs
+++ WebAPI/Models/UserModel.cs
@@ -7,4 +7,12 @@ public partial class UserModel
     public string? Email { get; set; }
 
     public int Id { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Phone { get; set; }
 }
